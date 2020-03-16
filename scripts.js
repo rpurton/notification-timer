307c68b565181c755ac74b79563a7110079d5e43
rpurton <rpurton87@gmail.com>
2020-03-15 17:09:46 -0700

    fixed visual counter - now displays time remaining during each interval

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 window.onload = function(){
 
   var a;
+  var counter;
 
   document.getElementById('buttons').addEventListener('click',function(evt) {
     var userMillis = Math.floor(60000 * document.getElementById('userminutes').value);
@@ -10,17 +11,16 @@ window.onload = function(){
     var target = evt.target;
     if (target.id === 'userstart') {
 
-      var counter = setInterval(function(){
+      counter = setInterval(counterFunc, 1000);
+
+      function counterFunc () {
         document.getElementById('timer').innerHTML = (seconds-1);
         seconds--
-        if (seconds === 0) {
-          document.getElementById('timer').innerHTML = '0';
-          clearInterval(counter);
+        //console.log(seconds);
+        //if (seconds === 0) {
+          //document.getElementById('timer').innerHTML = '0';
+          //clearInterval(counter);
         }
-      }, 1000);
-
-      // it's based on start button being pressed so it doesnt start over for each interval, only runs until first notify
-      //on notification send instead?? Notification.onshow? will recursive timeout work any better
 
       //browser request permission from user for notifications and logs result
       Notification.requestPermission().then(function(result) {
@@ -29,17 +29,20 @@ window.onload = function(){
 
       //start notification interval based on user inputted minutes
       a = setInterval(function(){
-        console.log('notify');
-        new Notification(document.getElementById('notifytext').value, {
-          body: 'It has been ' + document.getElementById('userminutes').value + ' minutes.',
-        });
-      }, userMillis);
+          console.log('notify');
+          new Notification(document.getElementById('notifytext').value, {
+            body: 'It has been ' + document.getElementById('userminutes').value + ' minutes.',
+          }
+          );
+          seconds = Math.floor(60 * document.getElementById('userminutes').value);
+          counter;
+        }, userMillis);
 
       //end notification interval when stop button is clicked
-    } else if (target.id === 'userstop') {
-      console.log('stop');
+    } if (target.id === 'userstop') {
+      console.log('stop notifying');
       clearInterval(a);
-
+      clearInterval(counter);
     } else;
 
   }, false);
