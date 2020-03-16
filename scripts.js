754d03c2f6b29029028b10218666efb4dae4d054
rpurton <rpurton87@gmail.com>
2020-03-15 17:14:47 -0700

    update code comments

--- scripts.js
+++ scripts.js
@@ -4,22 +4,20 @@ window.onload = function(){
   var counter;
 
   document.getElementById('buttons').addEventListener('click',function(evt) {
-    var userMillis = Math.floor(60000 * document.getElementById('userminutes').value);
 
+    var userMillis = Math.floor(60000 * document.getElementById('userminutes').value);
     var seconds = Math.floor(60 * document.getElementById('userminutes').value);
 
     var target = evt.target;
+
     if (target.id === 'userstart') {
 
+      //create visual timer in seconds
       counter = setInterval(counterFunc, 1000);
 
       function counterFunc () {
         document.getElementById('timer').innerHTML = (seconds-1);
         seconds--
-        //console.log(seconds);
-        //if (seconds === 0) {
-          //document.getElementById('timer').innerHTML = '0';
-          //clearInterval(counter);
         }
 
       //browser request permission from user for notifications and logs result
@@ -27,18 +25,17 @@ window.onload = function(){
         console.log(result);
       });
 
-      //start notification interval based on user inputted minutes
+      //start notification interval based on user inputted minutes; reset visual timer
       a = setInterval(function(){
           console.log('notify');
           new Notification(document.getElementById('notifytext').value, {
             body: 'It has been ' + document.getElementById('userminutes').value + ' minutes.',
-          }
-          );
+          });
           seconds = Math.floor(60 * document.getElementById('userminutes').value);
           counter;
         }, userMillis);
 
-      //end notification interval when stop button is clicked
+      //end notification interval and visual countdown when stop button is clicked
     } if (target.id === 'userstop') {
       console.log('stop notifying');
       clearInterval(a);
