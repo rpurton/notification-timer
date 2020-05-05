ba14d389ee10862e700febcdc95d2f21f846d559
rpurton <rpurton87@gmail.com>
2020-05-05 15:58:36 -0700

    fixed fields clearing on button press, which interfered with setInterval properly starting - button types were "submit" instead of "button"

--- scripts.js
+++ scripts.js
@@ -5,19 +5,27 @@ window.onload = function(){
 
   document.getElementById('buttons').addEventListener('click',function(evt) {
 
-    var userMillis = Math.floor(60000 * document.getElementById('userminutes').value);
-    var seconds = Math.floor(60 * document.getElementById('userminutes').value);
-
     var target = evt.target;
 
     if (target.id === 'userstart') {
+
+    var minutes = parseInt(document.getElementById('userminutes').value);
+    var hours = parseInt(document.getElementById('userhours').value);
+
+    var seconds = minutes * 60;
+    var millis =  minutes * 60000;
 
       //create visual timer in seconds
       counter = setInterval(counterFunc, 1000);
 
       function counterFunc () {
-        document.getElementById('timer').innerHTML = (seconds-1);
-        seconds--
+        //NOTE: somehow display a leading zero
+        //let displaySeconds = (seconds < 10) ? '0' + seconds : seconds;
+        //convert all userinput into seconds, add together, then output appropriately in visual counter
+
+        document.getElementById('timer').innerHTML =
+          "00" + ":" + "00" + ":" + (seconds - 1);
+        seconds--;
         }
 
       //browser request permission from user for notifications and logs result
@@ -29,11 +37,12 @@ window.onload = function(){
       a = setInterval(function(){
           console.log('notify');
           new Notification(document.getElementById('notifytext').value, {
-            body: 'It has been ' + document.getElementById('userminutes').value + ' minutes.',
+            body: 'It has been ' + minutes + ' minutes.',
+            //this needs work to output the correct time entered and plural
           });
-          seconds = Math.floor(60 * document.getElementById('userminutes').value);
+          seconds = 60 * minutes;
           counter;
-        }, userMillis);
+        }, millis);
 
       //end notification interval and visual countdown when stop button is clicked
     } if (target.id === 'userstop') {
